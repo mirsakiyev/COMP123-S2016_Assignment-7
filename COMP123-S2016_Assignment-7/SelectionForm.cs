f2a96bbd7af896dd582503a471da1cc1eef91342
mirsakiyev <mirsakiyev@yahoo.com>
2016-08-19 00:40:30 -0400

    modified

--- COMP123-S2016_Assignment-7/SelectionForm.cs
+++ COMP123-S2016_Assignment-7/SelectionForm.cs
@@ -42,11 +42,143 @@ namespace COMP123_S2016_Assignment_7
             {
                 TitleTextBox.Text = value.ToString();
 
+                if (value == "Season of the Witch")
+                {
+                    CategoryTextBox.Text = categories[3];
+                    CostTextBox.Text = "$" + cost[2].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.seasonofthewitch);                   
+                }
+
+                if (value == "The Green Hornet")
+                {
+                    CategoryTextBox.Text = categories[2];
+                    CostTextBox.Text = "$" + cost[2].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.thegreenhornet);
+                }
+
                 if (value == "The Dilemma")
                 {
-                    MoviePictureBox.Image = new Bitmap(Properties.Resources.thedilemma);
                     CategoryTextBox.Text = categories[0];
-                    CostTextBox.Text = "$"+cost[1].ToString();
+                    CostTextBox.Text = "$" + cost[1].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.thedilemma);
+                }
+
+                if (value == "Death Race 2")
+                {
+                    CategoryTextBox.Text = categories[2];
+                    CostTextBox.Text = "$" + cost[2].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.deathrace2);
+                }
+
+                if (value == "Company Men")
+                {
+                    CategoryTextBox.Text = categories[1];
+                    CostTextBox.Text = "$" + cost[1].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.companymen);
+                }
+
+                if (value == "No Strings Attached")
+                {
+                    CategoryTextBox.Text = categories[0];
+                    CostTextBox.Text = "$" + cost[1].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.nostringsattached);
+                }
+
+                if (value == "The Way Back")
+                {
+                    CategoryTextBox.Text = categories[1];
+                    CostTextBox.Text = "$" + cost[1].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.thewayback);
+                }
+
+                if (value == "The Mechanic")
+                {
+                    CategoryTextBox.Text = categories[2];
+                    CostTextBox.Text = "$" + cost[2].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.themechanic);
+                }
+
+                if (value == "The Rite")
+                {
+                    CategoryTextBox.Text = categories[4];
+                    CostTextBox.Text = "$" + cost[2].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.therite);
+                }
+
+                if (value == "Sanctum")
+                {
+                    CategoryTextBox.Text = categories[2];
+                    CostTextBox.Text = "$" + cost[2].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.sanctum);
+                }
+
+                if (value == "The Other Woman")
+                {
+                    CategoryTextBox.Text = categories[2];
+                    CostTextBox.Text = "$" + cost[2].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.theotherwoman);
+                }
+
+                if (value == "The Roommate")
+                {
+                    CategoryTextBox.Text = categories[5];
+                    CostTextBox.Text = "$" + cost[1].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.theroommate);
+                }
+
+                if (value == "Waiting for Forever")
+                {
+                    CategoryTextBox.Text = categories[1];
+                    CostTextBox.Text = "$" + cost[1].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.waitingforever);
+                }
+
+                if (value == "Cedar Rapids")
+                {
+                    CategoryTextBox.Text = categories[0];
+                    CostTextBox.Text = "$" + cost[1].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.cedarrapids);
+                }
+
+                if (value == "Gnomeo and Juliet")
+                {
+                    CategoryTextBox.Text = categories[6];
+                    CostTextBox.Text = "$" + cost[0].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.gnomeoandjuliet);
+                }
+
+                if (value == "Just Go With It")
+                {
+                    CategoryTextBox.Text = categories[0];
+                    CostTextBox.Text = "$" + cost[1].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.justgowithit);
+                }
+
+                if (value == "The Eagle")
+                {
+                    CategoryTextBox.Text = categories[2];
+                    CostTextBox.Text = "$" + cost[2].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.theeagle);
+                }
+
+                if (value == "I am Number Four")
+                {
+                    CategoryTextBox.Text = categories[3];
+                    CostTextBox.Text = "$" + cost[2].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.iamnumberfour);
+                }
+
+                if (value == "Footloose")
+                {
+                    CategoryTextBox.Text = categories[7];
+                    CostTextBox.Text = "$" + cost[3].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.footloose);
+                }
+                if (value == "Real Steel")
+                {
+                    CategoryTextBox.Text = categories[7];
+                    CostTextBox.Text = "$" + cost[3].ToString();
+                    MoviePictureBox.Image = new Bitmap(Properties.Resources.realsteel);
                 }
             }
         }
