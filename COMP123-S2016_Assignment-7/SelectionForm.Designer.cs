4ae7980307fa1eed62d378d2fb70fc8d6bff7af2
mirsakiyev <mirsakiyev@yahoo.com>
2016-08-18 23:17:37 -0400

    Added textboxes

--- COMP123-S2016_Assignment-7/SelectionForm.Designer.cs
+++ COMP123-S2016_Assignment-7/SelectionForm.Designer.cs
@@ -29,35 +29,92 @@
         private void InitializeComponent()
         {
             this.MovieListBox = new System.Windows.Forms.ListBox();
+            this.CurrentMoviesGroupBox = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.CurrentMoviesGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // MovieListBox
             // 
             this.MovieListBox.FormattingEnabled = true;
             this.MovieListBox.ItemHeight = 16;
-            this.MovieListBox.Location = new System.Drawing.Point(42, 88);
+            this.MovieListBox.Location = new System.Drawing.Point(19, 32);
             this.MovieListBox.Name = "MovieListBox";
             this.MovieListBox.Size = new System.Drawing.Size(245, 308);
             this.MovieListBox.Sorted = true;
             this.MovieListBox.TabIndex = 0;
+            // 
+            // CurrentMoviesGroupBox
+            // 
+            this.CurrentMoviesGroupBox.Controls.Add(this.MovieListBox);
+            this.CurrentMoviesGroupBox.Location = new System.Drawing.Point(12, 82);
+            this.CurrentMoviesGroupBox.Name = "CurrentMoviesGroupBox";
+            this.CurrentMoviesGroupBox.Size = new System.Drawing.Size(285, 359);
+            this.CurrentMoviesGroupBox.TabIndex = 1;
+            this.CurrentMoviesGroupBox.TabStop = false;
+            this.CurrentMoviesGroupBox.Text = "Current Movies";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(658, 133);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(658, 180);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(658, 228);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(342, 82);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 359);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // SelectionForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(782, 453);
-            this.Controls.Add(this.MovieListBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CurrentMoviesGroupBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "SelectionForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Selection Form";
+            this.CurrentMoviesGroupBox.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListBox MovieListBox;
+        private System.Windows.Forms.GroupBox CurrentMoviesGroupBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
 
